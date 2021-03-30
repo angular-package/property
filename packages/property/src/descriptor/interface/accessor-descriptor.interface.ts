b78ea016d68328f90417f06b66c21f3e1e5e8d68
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-30 22:42:16 +0200

    chore: add interface for accessor descriptor and its default constant value.

---
+++ packages/property/src/descriptor/interface/accessor-descriptor.interface.ts
@@ -0,0 +1,5 @@
+import { CommonDescriptor } from './common-descriptor.interface';
+export interface AccessorDescriptor<ValueType> extends CommonDescriptor {
+  get?: () => ValueType;
+  set?: (value: ValueType) => void;
+}
---
+++ packages/property/src/descriptor/lib/accessor-descriptor.const.ts
@@ -0,0 +1,5 @@
+import { AccessorDescriptor } from '../interface/accessor-descriptor.interface';
+export const ACCESSOR_DESCRIPTOR: AccessorDescriptor<any> = {
+  configurable: true,
+  enumerable: true
+};
