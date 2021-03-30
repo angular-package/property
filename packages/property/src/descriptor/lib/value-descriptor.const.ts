0bc03ab750832906bace6b80f9c6f04528c26efd
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-30 22:42:35 +0200

    chore(VALUE_DESCRIPTOR): add constant value for value descriptor.

---
+++ packages/property/src/descriptor/lib/value-descriptor.const.ts
@@ -0,0 +1,6 @@
+import { ValueDescriptor } from '../interface/value-descriptor.interface';
+export const VALUE_DESCRIPTOR: ValueDescriptor<any> = {
+  configurable: true,
+  enumerable: false,
+  writable: true
+};
