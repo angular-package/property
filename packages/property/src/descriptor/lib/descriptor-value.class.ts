aa0829ab45c156dc45d00063a4c45a2056e380eb
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-03-30 22:32:09 +0200

    feat(DescriptorValue): Create strict value descriptor for property.

---
+++ packages/property/src/descriptor/lib/descriptor-value.class.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { is, Types } from '@angular-package/type';
+// Internal.
+import { ValueDescriptor } from '../interface/value-descriptor.interface';
+import { VALUE_DESCRIPTOR } from './value-descriptor.const';
+
+@Injectable()
+export class DescriptorValue<ValueType = any> {
+  // Value descriptor.
+  private descriptor$$: ValueDescriptor<ValueType> = VALUE_DESCRIPTOR;
+
+  /**
+   * Get value descriptor.
+   */
+  get get(): ValueDescriptor<ValueType> {
+    return this.descriptor$$ as ValueDescriptor<ValueType>;
+  }
+
+  /**
+   * Creates instance.
+   * @param descriptor Value descriptor.
+   * @param type One of the types to check descriptor value.
+   */
+  constructor(descriptor: ValueDescriptor<ValueType>, type?: Types<ValueType>) {
+    this.set(descriptor, type);
+  }
+
+  /**
+   * Set value descriptor that means without accessors `get` `set` and `writable`.
+   * @param descriptor Value descriptor.
+   * @param type One of the types to check descriptor value.
+   * @returns this.
+   */
+  public set(descriptor: ValueDescriptor<ValueType>, type?: Types<ValueType>): this {
+    if (is.object<ValueDescriptor<ValueType>>(descriptor)) {
+      if (is.type<ValueType>(descriptor.value, type)) {
+        this.descriptor$$ = { ...this.descriptor$$, ...descriptor };
+      }
+    }
+    return this;
+  }
+}
