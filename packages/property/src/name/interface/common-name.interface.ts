c0e59005c5c98f32bc3a55a2add767228d1b5998
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-12 23:58:48 +0200

    refactor(CommonName): Remove config method parameter as optional.

--- packages/property/src/name/interface/common-name.interface.ts
+++ packages/property/src/name/interface/common-name.interface.ts
@@ -1,11 +1,9 @@
 import { ConfigName } from './config-name.interface';
-import { NamePrefix } from '../lib/name-prefix.class';
-import { NameSuffix } from '../lib/name-suffix.class';
 
 export interface CommonName {
   get: string;
   generate: string;
-  config: (config?: ConfigName) => this;
+  config: (config: ConfigName) => this;
   prefix: (value: string) => this;
   suffix: (value: string) => this;
 }
