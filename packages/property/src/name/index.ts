135103fbd1797e99144dc0aae893b3972d6d2f96
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-13 00:02:12 +0200

    chore(name): export all Name classes.

---
+++ packages/property/src/name/index.ts
@@ -0,0 +1,5 @@
+export { NameCommon } from './lib/name-common.class';
+export { NameConstant } from './lib/name-constant.class';
+export { NameGeneric } from './lib/name-generic.class';
+export { NamePrefix } from './lib/name-prefix.class';
+export { NameSuffix } from './lib/name-suffix.class';
