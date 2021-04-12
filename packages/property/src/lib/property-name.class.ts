0266443e0dd646a5804acec6f747a8604126c3c0
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-12 17:18:02 +0200

    feat(PropertyName): Class to create generic/constant property name with prefix and suffix.

---
+++ packages/property/src/lib/property-name.class.ts
@@ -0,0 +1,81 @@
+// External.
+import { is } from '@angular-package/type';
+// Internal.
+import { ConfigName } from '../name/interface/config-name.interface';
+import { NameConstant } from '../name/lib/name-constant.class';
+import { NameGeneric } from '../name/lib/name-generic.class';
+import { NameType } from '../name/type/name-type.type';
+import { PickName } from '../name/interface/pick-name.interface';
+
+export class PropertyName {
+  /**
+   * Properties.
+   * @public
+   */
+  public get generate(): string {
+    return this.pick?.generate || '';
+  }
+
+  public get get(): string {
+    return this.pick?.name || '';
+  }
+
+  public get pick(): PickName | undefined {
+    return this.detect?.pick;
+  }
+
+  /**
+   * Properties.
+   * @private
+   */
+  private get detect(): NameType | undefined {
+    return this.type<NameConstant>() || this.type<NameGeneric>();
+  }
+
+  #constant?: NameConstant;
+  #generic: NameGeneric = new NameGeneric();
+
+  /**
+   * Creates instance.
+   * @param constantOrConfig
+   * @param config
+   */
+  constructor(constantOrConfig?: string | ConfigName, config?: ConfigName) {
+    if (is.string(constantOrConfig)) {
+      this.#constant = new NameConstant(constantOrConfig, config);
+    } else {
+      this.#generic?.config(constantOrConfig);
+    }
+  }
+
+  /**
+   * Methods.
+   * @public
+   */
+  public config(config: ConfigName): this {
+    this.detect?.config(config);
+    return this;
+  }
+
+  public set(name: string): this {
+    if (is.instance<NameConstant>(this.#constant, NameConstant)) {
+      throw new Error(`Name is readonly.`);
+    }
+    this.#generic.set(name);
+    return this;
+  }
+
+  /**
+   * Methods.
+   * @private
+   */
+  private type<Type>(): Type | undefined {
+    if (is.object<Type>(this.#constant)) {
+      return this.#constant as Type;
+    }
+    if (is.object<Type>(this.#generic)) {
+      return this.#generic as Type;
+    }
+    return;
+  }
+}
