8783bd24db2f06745ee8eb71be8268d2474b6e4d
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-04-12 17:20:28 +0200

    refactor(NameConstant): Remove readonly property and use private on initial.

--- packages/property/src/name/lib/name-constant.class.ts
+++ packages/property/src/name/lib/name-constant.class.ts
@@ -1,13 +1,15 @@
-// External.
-import { guard } from '@angular-package/type';
 // Internal.
 import { NameCommon } from './name-common.class';
 import { ConfigName } from '../interface/config-name.interface';
+import { guard } from '@angular-package/type';
 
 export class NameConstant extends NameCommon {
-  readonly $name: string;
   constructor(name: string, config?: ConfigName) {
-    super(config);
-    this.$name = guard.is.string(name) ? name : '';
+    if (guard.is.string(name) === false) {
+      throw new Error(`
+        String \`name\` must be initialized.
+      `);
+    }
+    super(config, name);
   }
 }
