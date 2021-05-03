fff05a2288ea8996daf529e7b227ddc3052eaa4d
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-04 01:38:10 +0200

    chore(NameConstant): change error message, order and categorize imports, add description

--- packages/property/src/name/lib/name-constant.class.ts
+++ packages/property/src/name/lib/name-constant.class.ts
@@ -1,14 +1,16 @@
-// Internal.
-import { NameCommon } from './name-common.class';
-import { ConfigName } from '../interface/config-name.interface';
+// `guard` object.
 import { guard } from '@angular-package/type';
-
+// Class.
+import { NameCommon } from './name-common.class';
+// Interface.
+import { ConfigName } from '../interface/config-name.interface';
+/**
+ * Create a constant name with a changeable prefix and suffix.
+ */
 export class NameConstant extends NameCommon {
   constructor(name: string, config?: ConfigName) {
     if (guard.is.string(name) === false) {
-      throw new Error(`
-        String \`name\` must be initialized.
-      `);
+      throw new Error(`A \`string\` \`name\` must be initialized`);
     }
     super(config, name);
   }
