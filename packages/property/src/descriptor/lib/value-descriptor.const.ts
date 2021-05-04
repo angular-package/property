ee6196addd7fabda7583c215c6ab98ffcf2bad54
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-04 02:07:10 +0200

    refactor(VALUE_DESCRIPTOR): set default value to undefined

--- packages/property/src/descriptor/lib/value-descriptor.const.ts
+++ packages/property/src/descriptor/lib/value-descriptor.const.ts
@@ -2,5 +2,6 @@ import { ValueDescriptor } from '../interface/value-descriptor.interface';
 export const VALUE_DESCRIPTOR: ValueDescriptor<any> = {
   configurable: true,
   enumerable: false,
-  writable: true
+  writable: true,
+  value: undefined
 };
