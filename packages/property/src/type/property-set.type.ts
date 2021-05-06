dd21a3f3fe24bb2b0d7bd49413e8bcd3a2287fe2
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-06 21:03:02 +0200

    refactor(SetProperty): change name from `PropertySet`

--- packages/property/src/type/property-set.type.ts
+++
@@ -1,1 +0,0 @@
-export type PropertySet = <Obj, Key extends keyof Obj>(object: Obj, key: Key, value: Obj[Key]) => void;
---
+++ packages/property/src/type/set-property.type.ts
@@ -0,0 +1,1 @@
+export type SetProperty = <Obj extends object, Key extends keyof Obj>(object: Obj, key: Key, value: Obj[Key]) => Obj[Key] | undefined;
