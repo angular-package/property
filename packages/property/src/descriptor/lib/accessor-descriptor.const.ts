677ea27139e712242fb19d2f12641602d043633c
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-20 23:54:11 +0200

    chore(descriptor): default values for both descriptors

--- packages/property/src/descriptor/lib/accessor-descriptor.const.ts
+++ packages/property/src/descriptor/lib/accessor-descriptor.const.ts
@@ -2,6 +2,6 @@ import { AccessorDescriptor } from '../interface/accessor-descriptor.interface';
 export const ACCESSOR_DESCRIPTOR: AccessorDescriptor<any> = {
   configurable: true,
   enumerable: true,
-  get: () => {},
-  set: () => {}
+  get: undefined,
+  set: undefined
 };
