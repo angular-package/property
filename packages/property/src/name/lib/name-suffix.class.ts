da07d69c8d5fa8b5b1be3f8889afa40a53384131
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-21 04:34:53 +0200

    refactor(NameSuffix): add `callback`, update jsdoc

--- packages/property/src/name/lib/name-suffix.class.ts
+++ packages/property/src/name/lib/name-suffix.class.ts
@@ -1,5 +1,5 @@
 // Object.
-import { is } from '@angular-package/type';
+import { is, guard, ResultCallback } from '@angular-package/type';
 
 export class NameSuffix {
   // Get suffix.
@@ -12,19 +12,36 @@ export class NameSuffix {
 
   /**
    * Create instance.
-   * @param suffix Default `string` value as suffix.
+   * @param suffix An optional initial `string` type value as suffix.
    */
   constructor(suffix?: string) {
-    this.set(suffix);
+    if (is.string(suffix)) {
+      this.set(suffix);
+    }
+  }
+
+  /**
+   * Callback function for the `set` method.
+   * @param result A `boolean` type `result` of the check.
+   * @param value Any type `value` from the check.
+   * @returns A `boolean` indicating whether or not the suffix is a `string` type.
+   */
+  // TODO: Add errorCallback
+  public callback: ResultCallback = (result: boolean, value: any): boolean => {
+    if (result === false) {
+       throw new Error(`Suffix must be a \`string\` type, got value ${value}`);
+    }
+    return result;
   }
 
   /**
    * Set suffix for the name.
    * @param suffix A `string` type value.
+   * @param callback A `ResultCallback` function to handle the result of the check if the suffix is a `string`.
    * @returns this.
    */
-  public set(suffix?: string): this {
-    if (is.string(suffix)) {
+  public set(suffix: string, callback: ResultCallback = this.callback): this {
+    if (guard.is.string(suffix, callback)) {
       this.#suffix = suffix;
     }
     return this;
