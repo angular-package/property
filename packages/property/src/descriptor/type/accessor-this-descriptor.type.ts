290220505f5e7a3188d3a59e45a7c159f0dc6ce1
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-22 00:55:38 +0200

    docs(AccessorThisDescriptor): update jsdoc

--- packages/property/src/descriptor/type/accessor-this-descriptor.type.ts
+++ packages/property/src/descriptor/type/accessor-this-descriptor.type.ts
@@ -1,6 +1,6 @@
 import { AccessorDescriptor } from '../interface/accessor-descriptor.interface';
 /**
- * Accessor descriptor structure with the `Value`, `Obj` type extended with `ThisType<Obj>` object to use `this` properly its unique
+ * Accessor descriptor structure with the `Value`, `Obj` type extended with `ThisType<Obj>` object to use `this` properly in its unique
  * attributes.
  */
 export type AccessorThisDescriptor<Value, Obj = any> = AccessorDescriptor<Value> & ThisType<Obj>;
