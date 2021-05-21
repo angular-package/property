58ac28f8034344bcbcb6226c3d3e82a19596a2ff
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-22 01:40:30 +0200

    chore: remove unnecessary

--- packages/property/src/lib/property-in.function.ts
+++
@@ -1,3 +0,0 @@
-import { PropertyIn } from '../type/property-in.type';
-export const propertyIn: PropertyIn = <Obj>(properties: string[], object: Obj): boolean =>
-  properties.every(property => property in object === true);
