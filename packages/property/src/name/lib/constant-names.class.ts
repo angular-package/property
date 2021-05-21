5388b626d90c3d1858fc92d43201de3a2f17205a
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-21 23:56:26 +0200

    docs(ConstantNames): update jsdoc

--- packages/property/src/name/lib/constant-names.class.ts
+++ packages/property/src/name/lib/constant-names.class.ts
@@ -1,4 +1,4 @@
-// `guard` object.
+// Object.
 import { guard } from '@angular-package/type';
 // Class.
 import { CommonNames } from './common-names.class';
@@ -8,6 +8,11 @@ import { ConfigName } from '../interface/config-name.interface';
  * Create a constant name with a changeable prefix and suffix.
  */
 export class ConstantNames extends CommonNames {
+  /**
+   * Creates an instance for the constant/readonly name.
+   * @param name A `string` type value for the readonly `name`.
+   * @param config A `ConfigName` type value with `prefix` and `suffix` for the name.
+   */
   constructor(name: string, config?: ConfigName) {
     if (guard.is.string(name) === false) {
       throw new Error(`A \`string\` \`name\` must be initialized`);
