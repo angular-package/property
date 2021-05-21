e8906955aaea2d1b0ad2981555aa135f2acc621a
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-22 00:49:04 +0200

    docs(CommonDescriptor): update jsdoc

--- packages/property/src/descriptor/interface/common-descriptor.interface.ts
+++ packages/property/src/descriptor/interface/common-descriptor.interface.ts
@@ -1,5 +1,6 @@
 /**
- * Common attributes for accessor and data descriptor.
+ * Common `configurable` and `enumerable` of a `boolean` type attributes picked from default `PropertyDescriptor` for accessor and data
+ * descriptor.
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
  * "If a descriptor has neither of `value`, `writable`, `get` and `set` keys, it is treated as a data descriptor. "
  */
