9866ce9d040eb50121e78fab69ee815edc1478bf
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-22 01:43:27 +0200

    chore(descriptor): export main features

---
+++ packages/property/src/descriptor/index.ts
@@ -0,0 +1,12 @@
+// Class.
+export { AccessorDescriptors } from './lib/accessor-descriptors.class';
+export { DataDescriptors } from './lib/data-descriptors.class';
+export { Descriptor } from './lib/descriptor.class';
+export { GetOwnDescriptor } from './lib/get-own-descriptor.class';
+// Interface.
+export { AccessorDescriptor } from './interface/accessor-descriptor.interface';
+export { DataDescriptor } from './interface/data-descriptor.interface';
+// Type.
+export { AnyDescriptor } from './type/any-descriptor.type';
+export { AccessorThisDescriptor } from './type/accessor-this-descriptor.type';
+export { ObjectPropertyDescriptors } from './type/object-property-descriptors.type';
