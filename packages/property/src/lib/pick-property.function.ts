d04c04a331e4930dc9f4c77eb81c7fbd8e01e312
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-22 06:09:34 +0200

    refactor(pickProperty): use some shorthands and update jsdoc

--- packages/property/src/lib/pick-property.function.ts
+++ packages/property/src/lib/pick-property.function.ts
@@ -1,17 +1,12 @@
-import { PickProperty } from '../type/pick-property.type';
+// Object.
 import { is } from '@angular-package/type';
+// Type.
+import { PickProperty } from '../type/pick-property.type';
 /**
- * Pick properties from an object with specified keys.
- * @param obj Any type `obj` value to pick property from.
- * @param keys An `Array` of string values to pick from `obj`.
+ * Pick specified properties from an object.
+ * @param obj A generic `Obj` type `value` to pick the `keys` from.
+ * @param keys An `Array` of a key of `Obj` type values to pick from the `obj`.
+ * @returns An object with specified properties.
  */
-export const pickProperty: PickProperty = <Obj extends object>(obj: Obj, keys: (keyof Obj)[]): {[P in keyof Obj]: Obj[P]} => {
-  return Object.assign({}, ...keys.map(key => {
-    if (!is.undefined(obj[key])) {
-      return {
-        [key]: obj[key]
-      };
-    }
-    return;
-  }));
-};
+export const pickProperty: PickProperty = <Obj extends object>(obj: Obj, keys: (keyof Obj)[]): {[P in keyof Obj]: Obj[P]} =>
+  Object.assign({}, ...keys.map(key => !is.undefined(obj[key]) ? { [key]: obj[key] } : undefined ));
