c74c0e8cf930b4379489c5622ac80e66bca51da2
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-22 05:38:40 +0200

    feat(PickDescriptor): add type feature to pick in the class `Descriptor`

---
+++ packages/property/src/descriptor/interface/pick-descriptor.interface.ts
@@ -0,0 +1,12 @@
+import { GetOwnDescriptor } from '../lib/get-own-descriptor.class';
+// Interface.
+import { AccessorDescriptor } from './accessor-descriptor.interface';
+import { DataDescriptor } from './data-descriptor.interface';
+/**
+ * Pick `accessor`, `data` descriptor or class to get descriptor in `own`.
+ */
+export interface PickDescriptor<Value, Obj extends object> {
+  accessor: AccessorDescriptor<Value> & ThisType<Obj>;
+  data: DataDescriptor<Value>;
+  own: GetOwnDescriptor<Obj>;
+}
