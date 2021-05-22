d241fa9cc7ec60cc2aeff7f20fcf20d16ac8e973
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-22 05:39:29 +0200

    chore(descriptor): export types

---
+++ packages/property/src/descriptor/type/index.ts
@@ -0,0 +1,3 @@
+export { AnyDescriptor } from './any-descriptor.type';
+export { AccessorThisDescriptor } from './accessor-this-descriptor.type';
+export { ObjectPropertyDescriptors } from './object-property-descriptors.type';
