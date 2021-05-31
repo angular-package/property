f64f84a497b6cb3aa371b6e8834ac05cab6497d3
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-31 12:36:16 +0200

    refactor(descriptor): update `set()` by using `objectKeys()` method and jsdoc

--- packages/property/src/descriptor/lib/descriptor.class.ts
+++ packages/property/src/descriptor/lib/descriptor.class.ts
@@ -1,29 +1,26 @@
 // @angular-package
 import { Func, is, guard, ResultCallback, Key } from '@angular-package/type';
-// Classes.
+// Class.
 import { AccessorDescriptors } from './accessor-descriptors.class';
 import { DataDescriptors } from './data-descriptors.class';
-// Interfaces.
-// import { UnifiedDescriptor } from '../interface/unified-descriptor.interface';
-import { AccessorDescriptor } from '../interface/accessor-descriptor.interface';
+import { GetOwnDescriptor } from './get-own-descriptor.class';
+// Interface.
 import { DataDescriptor } from '../interface/data-descriptor.interface';
-import { GenericObject } from '../../interface/generic-object.interface';
 import { PickDescriptor } from '../interface/pick-descriptor.interface';
-// Types.
-// import { IsObject } from '@angular-package/type/is/type/is-object.type';
+// Type.
 import { AccessorThisDescriptor } from '../type/accessor-this-descriptor.type';
 import { AnyDescriptor } from '../type/any-descriptor.type';
-import { GetOwnDescriptor } from './get-own-descriptor.class';
-
-
+/**
+ * Methods to handle property descriptor or get it from the object.
+ */
 export class Descriptor<Value extends any, Obj extends object> {
-  // Accessor descriptor instance.
+  // Private accessor descriptor instance.
   #accessor: AccessorDescriptors<Value> = new AccessorDescriptors<Value, Obj>();
-  // Data descriptor instance.
+  // Private data descriptor instance.
   #data: DataDescriptors<Value> = new DataDescriptors<Value>();
-  // getOwn descriptor.
+  // Private get own descriptor instance.
   #getOwn: GetOwnDescriptor<Obj> = new GetOwnDescriptor();
-  // Get accessor or data descriptor depends on an existing instance.
+  // Get accessor, data descriptor or 'get own' descriptor instance.
   get get(): PickDescriptor<Value, Obj> {
     return {
       accessor: this.#accessor.get,
@@ -33,19 +30,19 @@ export class Descriptor<Value extends any, Obj extends object> {
   }
 
   /**
-   * Creates instance.
-   * @param descriptor Accessor or data descriptor to set.
+   * Creates `Descriptor<Value extends any, Obj extends object>` instance and an optionally set accessor descriptor.
+   * @param descriptor An optional `AnyDescriptor<Value, Obj>` type value to initially set.
    */
   constructor(descriptor?: AnyDescriptor<Value, Obj>) {
-    if (is.object<AnyDescriptor<Value, Obj>>(descriptor)) {
+    if (is.object(descriptor)) {
       this.set(descriptor);
     }
   }
 
   /**
-   * Set `AccessorThisDescriptor` type.
-   * @param descriptor A `AccessorThisDescriptor` type value.
-   * @returns this.
+   * Set accessor descriptor.
+   * @param descriptor A `AccessorThisDescriptor<Value, Obj>` type value.
+   * @returns A `Descriptor` instance.
    */
    public accessor(descriptor: AccessorThisDescriptor<Value, Obj>): this {
     this.#accessor.set(descriptor);
@@ -53,9 +50,9 @@ export class Descriptor<Value extends any, Obj extends object> {
   }
 
   /**
-   * Set `DataDescriptor` type.
+   * Set data descriptor.
    * @param descriptor A `DataDescriptor` type value.
-   * @returns this.
+   * @returns A `Descriptor` instance.
    */
    public data(descriptor: DataDescriptor<Value>): this {
     this.#data.set(descriptor);
@@ -63,90 +60,18 @@ export class Descriptor<Value extends any, Obj extends object> {
   }
 
   /**
-   * Set `AccessorThisDescriptor` or `DataDescriptor`
+   * Set accessor or data descriptor depends on detected properties.
    * @param descriptor A `AnyDescriptor` type value.
    * @param callback
-   * @returns this.
+   * @returns A `Descriptor` instance.
    */
-   public set(descriptor: AnyDescriptor<Value, Obj>, callback?: ResultCallback): this {
+  public set(descriptor: AnyDescriptor<Value, Obj>, callback?: ResultCallback): this {
     // TODO: check the value type
-    if (is.objectKey<AccessorThisDescriptor<Value, Obj>>(descriptor, ['get', 'set'], callback)) {
-      this.accessor(descriptor);
-    } else if (is.objectKey<DataDescriptor<Value>>(descriptor, 'value')) {
-      this.data(descriptor);
+    if (is.objectKeys<AccessorThisDescriptor<Value, Obj>>(descriptor, 'get', 'set')) {
+      this.#accessor.set(descriptor, callback);
+    } else if (is.objectKeys<DataDescriptor<Value>>(descriptor, 'writable', 'value')) {
+      this.#data.set(descriptor, callback);
     }
     return this;
   }
 }
-
-class Person {
-  #firstName = '';
-  #surname = 'bla';
-
-  age = 5;
-  private city = 'Poznań';
-
-  get _city(): string {
-    return this.city;
-  }
-
-  get firstName(): string {
-    return this.#firstName;
-  }
-  set firstName(value: string) {
-    this.#firstName = value;
-  }
-  get surname(): string {
-    return this.#surname;
-  }
-  set surname(value: string) {
-    this.#surname = value;
-  }
-}
-
-const p: Person = new Person();
-
-const descriptoR: Descriptor<string, Person> = new Descriptor();
-console.log(Object.getOwnPropertyDescriptors(Object.getPrototypeOf(p)));
-console.log(descriptoR.get.own.property('firstName'));
-
-// public type<Type = TypeDescriptor<ValueType>>(): Type {
-//   if (is.object<Type>(this.accessor$$)) {
-//     return this.accessor$$.get as Type;
-//   } else if (is.object<Type>(this.value$$)) {
-//     return this.value$$.get as Type;
-//   }
-// }
-
-export function Lookup<Obj extends object>(property: keyof Obj): Func {
-  return (component: Obj): any => {
-    const descriptor: GetOwnDescriptor<Obj> = new GetOwnDescriptor();
-    // console.log(`Lookup is class: `, is.class(component));
-    // console.log(`Lookup: `, descriptor.property(property, component));
-  };
-}
-
-  // /**
-  //  * Return an object property descriptor.
-  //  * @param object Generic `Obj` type value that contains `property`.
-  //  * @returns An object properties descriptors.
-  //  */
-  //  public object<O extends Obj>(object: O): {[P in keyof O]: TypedPropertyDescriptor<O[P]>} & { [x: string]: PropertyDescriptor } {
-  //   const detectedObject: O = is.function(object) ? object.prototype : is.object<O>(object) ? object : undefined ;
-  //   return Object.getOwnPropertyDescriptors<O>(detectedObject);
-  // }
-
-  // /**
-  //  * Return object property descriptor.
-  //  * @param object Generic `Obj` type value that contains the `key`.
-  //  * @param key A `Key` type name of the property that `object` contains.
-  //  * @returns A `PropertyDescriptor` type.
-  //  */
-  //  public property(object: Obj, key: Key): PropertyDescriptor | undefined {
-  //   const detectedObject: Func | Obj = is.function(object) ? object.prototype : is.object<Obj>(object) ? object : undefined ;
-  //   return !is.undefined(detectedObject)
-  //     ? guard.is.key(key)
-  //       ? Object.getOwnPropertyDescriptor(detectedObject, key)
-  //     : undefined
-  //   : undefined;
-  // }
