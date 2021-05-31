62865a2078458e7b863e44f09d5c5ee2c8d4ab75
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-31 14:24:14 +0200

    refactor(getProperty): add export default function as GetProperty to check `object` and `key`

--- packages/property/src/lib/get-property.function.ts
+++ packages/property/src/lib/get-property.function.ts
@@ -1,9 +1,27 @@
+// @angular-package/type
+import { guard, ResultCallback } from '@angular-package/type';
 // Type.
 import { GetProperty } from '../type/get-property.type';
+
 /**
  * Get property from the object.
- * @param object An `obj` type value to 
- * @param key 
- * @returns 
+ * @param object An `obj` type value to
+ * @param key x
+ * @returns x
  */
 export const getProperty: GetProperty = <Obj extends object, Key extends keyof Obj>(object: Obj, key: Key): Obj[Key] => object[key];
+
+/**
+ *
+ * @param object
+ * @param key
+ * @param callback
+ * @returns
+ */
+export default
+  (<Obj extends object, Key extends keyof Obj>(object: Obj, key: Key, callback?: ResultCallback): Obj[Key] | undefined => {
+    if (guard.is.objectKey(object, key, callback)) {
+      return object[key];
+    }
+    return;
+  }) as GetProperty;
