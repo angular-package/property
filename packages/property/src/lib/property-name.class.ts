197faf7d3c14f69a4d3f4ee87b15f2c34ca86935
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-31 14:21:34 +0200

    refactor(PropertyName): update
    - remove private `type()` method
    - change `set()` method and use `ConstantNames` from `NameConstant`
    - remove `?` cause tests problems

--- packages/property/src/lib/property-name.class.ts
+++ packages/property/src/lib/property-name.class.ts
@@ -1,83 +1,67 @@
-// External.
+// Object.
 import { is } from '@angular-package/type';
-// Internal.
-import { NameConstant } from '../name/lib/name-constant.class';
-import { NameGeneric } from '../name/lib/name-generic.class';
+// Class.
+import { ConstantNames } from '../name/lib/constant-names.class';
+import { GenericNames } from '../name/lib/generic-names.class';
 // Interface.
 import { ConfigName } from '../name/interface/config-name.interface';
 import { PickName } from '../name/interface/pick-name.interface';
+import { GenericConfigName } from '../name/interface/generic-config-name.interface';
 // Type.
-import { NameType } from '../name/type/name-type.type';
-
+import { Names } from '../name/type/names.type';
+/**
+ * Class to handle string type property name.
+ */
 export class PropertyName {
-  /**
-   * Properties.
-   * @public
-   */
   public get generate(): string {
-    return this.pick?.generate || '';
+    return this.pick.generate;
   }
 
   public get get(): string {
-    return this.pick?.name || '';
+    return this.pick.name;
   }
 
-  public get pick(): PickName | undefined {
-    return this.detect?.pick;
+  public get pick(): PickName {
+    return this.detect.pick;
   }
 
-  /**
-   * Properties.
-   * @private
-   */
-  private get detect(): NameType | undefined {
-    return this.type<NameConstant>() || this.type<NameGeneric>();
+  private get detect(): Names {
+    return this.#constant || this.#generic;
   }
 
-  #constant?: NameConstant;
-  #generic: NameGeneric = new NameGeneric();
+  #constant?: ConstantNames;
+  #generic: GenericNames = new GenericNames();
 
-  /**
-   * Creates instance.
-   * @param constantOrConfig
-   * @param config
-   */
-  constructor(constantOrConfig?: string | ConfigName, config?: ConfigName) {
+  constructor(constantOrConfig?: string | GenericConfigName, config?: ConfigName) {
     if (is.string(constantOrConfig)) {
-      this.#constant = new NameConstant(constantOrConfig, config);
-    } else {
-      this.#generic?.config(constantOrConfig);
+      this.#constant = new ConstantNames(constantOrConfig, config);
+    } else if (is.object<GenericConfigName>(constantOrConfig)) {
+      this.config(constantOrConfig);
+      if (is.objectKey<GenericConfigName>(constantOrConfig, ['name'])) {
+        if (is.string(constantOrConfig.name)) {
+          this.set(constantOrConfig.name);
+        }
+      }
     }
   }
 
-  /**
-   * Methods.
-   * @public
-   */
   public config(config: ConfigName): this {
-    this.detect?.config(config);
+    if (is.object(this.detect)) {
+      if (is.string(config.prefix)) {
+        this.detect.prefix(config.prefix);
+      }
+      if (is.string(config.suffix)) {
+        this.detect.suffix(config.suffix);
+      }
+    }
     return this;
   }
 
   public set(name: string): this {
-    if (is.instance<NameConstant>(this.#constant, NameConstant)) {
-      throw new Error(`Name is readonly.`);
+    if (is.instance<ConstantNames>(this.#constant, ConstantNames)) {
+      throw new Error(`Name ${this.#constant.get} is readonly`);
     }
     this.#generic.set(name);
     return this;
-  }
-
-  /**
-   * Methods.
-   * @private
-   */
-  private type<Type>(): Type | undefined {
-    if (is.object<Type>(this.#constant)) {
-      return this.#constant as Type;
-    }
-    if (is.object<Type>(this.#generic)) {
-      return this.#generic as Type;
-    }
-    return;
   }
 }
