3145b993b86ecaa79ad3cdd0bf6f8bbc5cab9122
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-04 20:05:16 +0200

    chore: remove unnecessary constants

--- src/descriptor/lib/accessor-descriptor.const.ts
+++
@@ -1,7 +0,0 @@
-import { AccessorDescriptor } from '../interface/accessor-descriptor.interface';
-export const ACCESSOR_DESCRIPTOR: AccessorDescriptor<any> = {
-  configurable: true,
-  enumerable: true,
-  get: undefined,
-  set: undefined
-};
--- src/descriptor/lib/data-descriptor.const.ts
+++
@@ -1,7 +0,0 @@
-import { DataDescriptor } from '../interface/data-descriptor.interface';
-export const DATA_DESCRIPTOR: DataDescriptor<any> = {
-  configurable: true,
-  enumerable: false,
-  writable: true,
-  value: undefined
-};
