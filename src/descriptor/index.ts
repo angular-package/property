976e5ab089b69cc403614e1f8cc863cf954a10e5
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-04 20:16:30 +0200

    chore(descriptor): index update export types instead of interfaces

--- src/descriptor/index.ts
+++ src/descriptor/index.ts
@@ -3,10 +3,8 @@ export { AccessorDescriptors } from './lib/accessor-descriptors.class';
 export { DataDescriptors } from './lib/data-descriptors.class';
 export { Descriptor } from './lib/descriptor.class';
 export { GetOwnDescriptor } from './lib/get-own-descriptor.class';
-// Interface.
-export { AccessorDescriptor } from './interface/accessor-descriptor.interface';
-export { DataDescriptor } from './interface/data-descriptor.interface';
 // Type.
+export { AccessorDescriptor } from './type/accessor-descriptor.type';
 export { AnyDescriptor } from './type/any-descriptor.type';
-export { AccessorThisDescriptor } from './type/accessor-this-descriptor.type';
+export { DataDescriptor } from './type/data-descriptor.type';
 export { ObjectPropertyDescriptors } from './type/object-property-descriptors.type';
