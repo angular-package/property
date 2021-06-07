72023b7993fb57bf7162b7448ad04e4c0929cde8
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-07 18:49:21 +0200

    refactor(Name): update exports

--- src/name/index.ts
+++ src/name/index.ts
@@ -1,10 +1,9 @@
 // Class.
-export { CommonNames } from './lib/common-names.class';
-export { ConstantNames } from './lib/constant-names.class';
-export { GenericNames } from './lib/generic-names.class';
-export { NamePrefix } from './lib/name-prefix.class';
-export { NameSuffix } from './lib/name-suffix.class';
-
+export { CommonName } from './lib/common-names.class';
+export { ConstantName } from './lib/constant-names.class';
+export { Name } from './lib/name.class';
+export { Prefix } from './lib/prefix.class';
+export { Suffix } from './lib/suffix.class';
 // Interface.
 export { ConfigName } from './interface/config-name.interface';
 export { GenericConfigName } from './interface/generic-config-name.interface';
