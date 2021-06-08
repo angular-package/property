dfbb0aa26bc9f48de93b0525fdb74c1e6916176b
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-08 20:20:29 +0200

    docs(ThisAccessorDescriptor): update jsdoc and use prettiezer

--- src/descriptor/type/this-accessor-descriptor.type.ts
+++ src/descriptor/type/this-accessor-descriptor.type.ts
@@ -1,5 +1,6 @@
 import { AccessorDescriptor } from '../interface/accessor-descriptor.interface';
 /**
- * `AccessorDescriptor` type with ease of use `this` of an `Obj` type in the `get()` and `set()` functions.
+ * `AccessorDescriptor` interface as a type cause of ease of use `this` of an `Obj` type in the `get()` and `set()` functions.
  */
-export type ThisAccessorDescriptor<Value, Obj> = AccessorDescriptor<Value> & ThisType<Obj>;
+export type ThisAccessorDescriptor<Value, Obj> = AccessorDescriptor<Value> &
+  ThisType<Obj>;
