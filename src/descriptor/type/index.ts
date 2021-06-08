b930852b29aeb7dd7d0215718210852690d47123
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-08 20:15:45 +0200

    chore(type/index): export only public

--- src/descriptor/type/index.ts
+++ src/descriptor/type/index.ts
@@ -1,4 +1,2 @@
-export { AccessorDescriptor } from '../type/accessor-descriptor.type';
-export { AnyDescriptor } from './any-descriptor.type';
-export { DataDescriptor } from '../type/data-descriptor.type';
-export { ObjectPropertyDescriptors } from './object-property-descriptors.type';
+// Public.
+export { ThisAccessorDescriptor } from './this-accessor-descriptor.type';
