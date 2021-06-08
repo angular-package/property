631dcb0ce79a0e39c811cc41d27bf48f67cb5572
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-08 20:13:19 +0200

    style(CommonDescriptor): use prettiezer

--- src/descriptor/interface/common-descriptor.interface.ts
+++ src/descriptor/interface/common-descriptor.interface.ts
@@ -3,4 +3,5 @@
  * picked from the default `PropertyDescriptor`.
  * "If a descriptor has neither of `value`, `writable`, `get` and `set` keys, it is treated as a data descriptor. "
  */
-export interface CommonDescriptor extends Pick<PropertyDescriptor, 'configurable' | 'enumerable'> {}
+export interface CommonDescriptor
+  extends Pick<PropertyDescriptor, 'configurable' | 'enumerable'> {}
