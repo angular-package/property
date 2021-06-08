1ec1241b0ad2426f532e929ccd333be83ad103d0
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-08 14:35:27 +0200

    refactor(DataDescriptor): back to interface

--- src/descriptor/interface/data-descriptor.interface.ts
+++ src/descriptor/interface/data-descriptor.interface.ts
@@ -1,8 +1,8 @@
-import { CommonDescriptor } from './common-descriptor.type';
+import { CommonDescriptor } from './common-descriptor.interface';
 /**
  * Descriptor with its unique optional keys, `writable` of a `boolean` type and `value` of a generic `Value` type.
  */
-export type DataDescriptor<Value> = CommonDescriptor & {
+export interface DataDescriptor<Value> extends CommonDescriptor {
   writable?: boolean;
   value?: Value;
-};
+}
