38fa208852b61d284ee1aedb571d206351cd2b9c
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-08 20:12:56 +0200

    docs(AccessorDescriptor): update jsdoc

--- src/descriptor/interface/accessor-descriptor.interface.ts
+++ src/descriptor/interface/accessor-descriptor.interface.ts
@@ -1,6 +1,7 @@
 import { CommonDescriptor } from './common-descriptor.interface';
 /**
- * Descriptor with its unique optional `get` and `set` keys of a function type, with the specified `Value` type.
+ * Descriptor with its unique optional `get()` and `set()` functions, of the `Value` type.
+ * For the accessor descriptor with also the object type, please use the type `ThisAccessorDescriptor<Value, Obj>`.
  */
 export interface AccessorDescriptor<Value> extends CommonDescriptor {
   get?: () => Value;
