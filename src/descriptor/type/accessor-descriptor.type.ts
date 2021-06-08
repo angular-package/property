2f271314f41226dfdc83e5e96c9dd8ee915d46fe
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-08 14:33:08 +0200

    refactor(AccessorDescriptor): back to interface

--- src/descriptor/type/accessor-descriptor.type.ts
+++
@@ -1,8 +0,0 @@
-import { CommonDescriptor } from './common-descriptor.type';
-/**
- * Descriptor with its unique optional `get` and `set` keys of a function type, with the specified `Value` type.
- */
-export type AccessorDescriptor<Value, Obj = any> = CommonDescriptor & {
-  get?: () => Value;
-  set?: (value: Value) => void;
-} & ThisType<Obj>;
