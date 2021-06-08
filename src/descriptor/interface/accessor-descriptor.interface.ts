3bb9315bcf82d2f0fe10256befe53d71bea6c503
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-08 14:45:56 +0200

    refactor(AccessorDescriptor): remove `Obj`

--- src/descriptor/interface/accessor-descriptor.interface.ts
+++ src/descriptor/interface/accessor-descriptor.interface.ts
@@ -2,7 +2,7 @@ import { CommonDescriptor } from './common-descriptor.interface';
 /**
  * Descriptor with its unique optional `get` and `set` keys of a function type, with the specified `Value` type.
  */
-export interface AccessorDescriptor<Value, Obj = any> extends CommonDescriptor {
+export interface AccessorDescriptor<Value> extends CommonDescriptor {
   get?: () => Value;
   set?: (value: Value) => void;
 }
