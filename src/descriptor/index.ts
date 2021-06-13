5c7acd3691416b288157bf0c9cdab69a4cfae3b5
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-14 00:43:04 +0200

    chore(descriptor/index): remove `OwnDescriptor`

--- src/descriptor/index.ts
+++ src/descriptor/index.ts
@@ -2,7 +2,6 @@
 export { AccessorDescriptors } from './lib/accessor-descriptors.class';
 export { DataDescriptors } from './lib/data-descriptors.class';
 export { Descriptor } from './lib/descriptor.class';
-export { OwnDescriptor } from './lib/get-own-descriptor.class';
 // Interface.
 export { AccessorDescriptor } from './interface/accessor-descriptor.interface';
 export { DataDescriptor } from './interface/data-descriptor.interface';
