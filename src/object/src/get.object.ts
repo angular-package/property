6f5360395ac4dda5baa5f2e070a5e240fc31bba7
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-14 01:18:25 +0200

    chore(object): move to separate folder

---
+++ src/object/src/get.object.ts
@@ -0,0 +1,10 @@
+// Function.
+import { getObject } from '../../lib/get-object.function';
+import { getProperty } from '../../lib/get-property.function';
+// Interface.
+import { Get } from '../../interface/get.interface';
+
+export const get: Get = {
+  object: getObject,
+  property: getProperty
+};
