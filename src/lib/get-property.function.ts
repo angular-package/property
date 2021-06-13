219bae2b0c9fe0e8cb362feb16b60905e97c668d
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-13 20:51:03 +0200

    chore(getProperty()): use prettiezer on code, update jsdoc

--- src/lib/get-property.function.ts
+++ src/lib/get-property.function.ts
@@ -1,9 +1,15 @@
 // Type.
 import { GetProperty } from '../type/get-property.type';
 /**
- * Get property from the object.
- * @param object An `obj` type value to
- * @param key x
- * @returns x
+ * Returns the value of the specified property from the `object`.
+ * @param object An `object` of a generic `Obj` type to get property value from.
+ * @param key A `keyof Obj` type value, as the name of the property that an `object` contains.
+ * @returns The return value is an undefined or a property value from the `object`.
  */
-export const getProperty: GetProperty = <Obj extends object, Key extends keyof Obj>(object: Obj, key: Key): Obj[Key] => object[key];
+export const getProperty: GetProperty = <
+  Obj extends object,
+  Key extends keyof Obj
+>(
+  object: Obj,
+  key: Key
+): Obj[Key] => object[key];
