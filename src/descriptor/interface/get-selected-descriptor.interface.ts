dcf8ff3724ac56276527e36ece38bfa468ea9ecc
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-14 14:56:34 +0200

    feat(GetSelectedDescriptor): add `from` property to get descriptor from the `object` or property of the object

--- src/descriptor/interface/get-selected-descriptor.interface.ts
+++ src/descriptor/interface/get-selected-descriptor.interface.ts
@@ -1,10 +1,17 @@
 // Interface.
-import { DataDescriptor } from '../interface/data-descriptor.interface';
+import { DataDescriptor } from './data-descriptor.interface';
 import { ThisAccessorDescriptor } from '../type/this-accessor-descriptor.type';
+import { ObjectPropertyDescriptors } from '../type/object-property-descriptors.type';
+// Type.
+import { Func } from '@angular-package/type';
 /**
- * Get accessor or data descriptor.
+ * Get stored accessor or data descriptor, or from the object, or from the specified object property.
  */
 export interface GetSelectedDescriptor<Value, Obj extends object> {
   accessor: ThisAccessorDescriptor<Value, Obj>;
   data: DataDescriptor<Value>;
+  from: {
+    object: (object: Obj) => ObjectPropertyDescriptors<Obj> | undefined;
+    property: (object: object | Func, key: keyof Obj) => PropertyDescriptor | undefined;
+  };
 }
