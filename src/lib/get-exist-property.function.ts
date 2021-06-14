c6b03e5cb9254f75a2e13050c90f1478b4e244b8
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-14 03:14:44 +0200

    refactor(getExistProperty()): change the way of checking existence of the object property and update jsdoc

--- src/lib/get-exist-property.function.ts
+++ src/lib/get-exist-property.function.ts
@@ -2,17 +2,23 @@
 import { guard, ResultCallback } from '@angular-package/type';
 // Type.
 import { GetProperty } from '../type/get-property.type';
+import { getProperty } from './get-property.function';
 /**
- *
- * @param object
- * @param key
- * @param callback
- * @returns
+ * Returns the value of the existing specified property from the `object`.
+ * @param object An `object` of a generic `Obj` type, by default of the type captured from the provided `object`,
+ * to get the existing property value from. The value is being checked against the proper `object` type
+ * @param key A `keyof` type property name from the existing `object`, by default of type captured from the provided `key`
+ * as the name of the property that the `object` contains. The value is being checked against existence the property in the `object`.
+ * @returns The return value is a property value from the `object`.
  */
-export const getExistProperty: GetProperty =
-  <Obj extends object, Key extends keyof Obj>(object: Obj, key: Key, callback?: ResultCallback): Obj[Key] | undefined => {
-    if (guard.is.objectKey(object, key, callback)) {
-      return object[key];
-    }
-    return;
-  };
+export const getExistProperty: GetProperty = <
+  Obj extends object,
+  Key extends keyof Obj
+>(
+  object: Obj,
+  key: Key,
+  callback?: ResultCallback
+): Obj[Key] =>
+  guard.is.objectKey(object, key, callback)
+    ? getProperty(object, key)
+    : getProperty(object, key);
