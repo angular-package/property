19987a3db2a0df42e85a49521c3de3c5f83991fb
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-14 14:11:43 +0200

    chore(callbacks): move to callback directory

--- src/callback/src/callback.object.ts
+++ src/callback/src/callback.object.ts
@@ -1,8 +1,10 @@
-import { errorCallback } from '../callback/src/error-callback.function';
-
+import { errorCallback } from './error-callback.function';
+/**
+ * Object with all necessary callbacks for the property package.
+ */
 export const callbacks = {
   accessor: errorCallback(`Accessor descriptor must be an \`ThisAccessorDescriptor<Value, Obj>\` type`),
   data: errorCallback(`Data descriptor must be an \`DataDescriptors<Value>\` type`),
   descriptor: errorCallback(`Any kind of descriptor was not found`),
-  getExistProperty: errorCallback(`Object of the specified key does not exist`, false),
+  getExistProperty: errorCallback(`Object with the specified key does not exist`, false),
 };
