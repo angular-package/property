b3cf8350d2328f8e779265827753ac98946e6b91
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-20 21:55:03 +0200

    refactor(GetDescriptors): change the `key` parameter to `keys` and add generic variable `Keys`

--- src/descriptor/type/get-descriptors.type.ts
+++ src/descriptor/type/get-descriptors.type.ts
@@ -1,2 +1,11 @@
+// @angular-package/type
+import { Func } from '@angular-package/type';
+// Type.
 import { ObjectPropertyDescriptors } from './object-property-descriptors.type';
-export type GetDescriptors = <Obj>(object: Obj, key?: (keyof Obj | keyof Obj[])) => ObjectPropertyDescriptors<Obj> | undefined;
+export type GetDescriptors = <
+  Obj extends object | Func,
+  Keys extends keyof Obj
+>(
+  object: Obj,
+  keys?: Keys[]
+) => ObjectPropertyDescriptors<Obj> | undefined;
