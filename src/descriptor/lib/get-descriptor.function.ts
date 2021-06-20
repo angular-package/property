d5b87e5bf05a96acdbe99ac330fb434e336794af
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-21 01:43:54 +0200

    docs(getDescriptor): update jsdoc

--- src/descriptor/lib/get-descriptor.function.ts
+++ src/descriptor/lib/get-descriptor.function.ts
@@ -1,4 +1,10 @@
 import { GetDescriptor } from '../type/get-descriptor.type';
+/**
+ * Wrapper function for the `Object` static method `getOwnPropertyDescriptor`.
+ * @param object "Object that contains the property."
+ * @param key "Name of the property."
+ * @returns The return value is a property descriptor of the specified object.
+ */
 export const getDescriptor: GetDescriptor = <Obj, Key extends keyof Obj>(
   object: Obj,
   key: Key
