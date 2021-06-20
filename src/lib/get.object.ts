16ec2bee4798d40286df48b1a490361d6eb7e6b2
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-20 19:01:49 +0200

    feat(get.object): add `object()` feature to the `get` object

--- src/lib/get.object.ts
+++ src/lib/get.object.ts
@@ -1,12 +1,14 @@
 // Function.
 import { getExistProperty } from './get-exist-property.function';
+import { getObject } from '../object/src/get-object.function';
 import { getProperties } from './get-properties.function';
 import { getProperty } from './get-property.function';
 // Type.
 import { Get } from '../type/get.type';
-// Object.
+// Export object.
 export const get: Get = {
   existProperty: getExistProperty,
+  object: getObject,
   properties: getProperties,
   property: getProperty
 };
--- src/type/get.type.ts
+++ src/type/get.type.ts
@@ -1,9 +1,12 @@
+// Type.
 import { GetExistProperty } from '../type/get-exist-property.type';
+import { GetObject } from './get-object.type';
 import { GetProperties } from '../type/get-properties.type';
 import { GetProperty } from '../type/get-property.type';
-
+// Export interface.
 export interface Get {
   existProperty: GetExistProperty;
+  object: GetObject;
   properties: GetProperties;
   property: GetProperty;
 }
