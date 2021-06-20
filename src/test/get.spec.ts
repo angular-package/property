8908de5677f4ae9cd1cc569b8fc9745bb67cee99
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-20 21:53:28 +0200

    test(get): add `descriptor()` and `descriptors()` to check against defined

--- src/test/get.spec.ts
+++ src/test/get.spec.ts
@@ -8,6 +8,8 @@ describe(`get object`, () => {
   it('is DEFINED', () => expect(get).toBeDefined());
 
   describe(`defined`, () => {
+    it('`descriptor()`', () => expect(get.descriptor).toBeDefined());
+    it('`descriptors()`', () => expect(get.descriptors).toBeDefined());
     it('`existProperty()`', () => expect(get.existProperty).toBeDefined());
     it('`object()`', () => expect(get.object).toBeDefined());
     it('`properties()`', () => expect(get.properties).toBeDefined());
