52a9095927a14de46a61b6999bdfbcc6e0e2c0c5
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-20 21:52:07 +0200

    feat(get): add to object `descriptor()` and `descriptors()` function

--- src/lib/get.object.ts
+++ src/lib/get.object.ts
@@ -1,4 +1,6 @@
 // Function.
+import { getDescriptor } from '../descriptor/lib/get-descriptor.function';
+import { getDescriptors } from '../descriptor/lib/get-descriptors.function';
 import { getExistProperty } from './get-exist-property.function';
 import { getObject } from '../object/src/get-object.function';
 import { getProperties } from './get-properties.function';
@@ -6,7 +8,12 @@ import { getProperty } from './get-property.function';
 // Type.
 import { Get } from '../type/get.type';
 // Export object.
+/**
+ * Get object with all the get functions.
+ */
 export const get: Get = {
+  descriptor: getDescriptor,
+  descriptors: getDescriptors,
   existProperty: getExistProperty,
   object: getObject,
   properties: getProperties,
