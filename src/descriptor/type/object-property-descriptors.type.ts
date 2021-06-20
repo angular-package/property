989d281bcca610aec0e9f3f9eebe9425f76e96a9
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-20 21:52:40 +0200

    style(ObjectPropertyDescriptors): use prettiezer

--- src/descriptor/type/object-property-descriptors.type.ts
+++ src/descriptor/type/object-property-descriptors.type.ts
@@ -1,1 +1,3 @@
-export type ObjectPropertyDescriptors<Obj> = {[P in keyof Obj]: TypedPropertyDescriptor<Obj[P]>} & { [x: string]: PropertyDescriptor };
+export type ObjectPropertyDescriptors<Obj> = {
+  [P in keyof Obj]: TypedPropertyDescriptor<Obj[P]>;
+} & { [x: string]: PropertyDescriptor };
