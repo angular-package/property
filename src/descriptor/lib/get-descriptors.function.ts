08e0fe65772be9165c4e2886af0c95d58155f29d
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-22 00:01:05 +0200

    docs(getDescriptors()): update jsdoc

--- src/descriptor/lib/get-descriptors.function.ts
+++ src/descriptor/lib/get-descriptors.function.ts
@@ -3,7 +3,9 @@ import { ObjectPropertyDescriptors } from '../type/object-property-descriptors.t
 /**
  * Wrapper function for the `Object` static method `getOwnPropertyDescriptors`.
  * "Returns an object containing all own property descriptors of an object".
- * @param object "Object that contains the properties and methods.
+ * @param object An `object` of a generic `Obj` type, by default of the type captured from the provided `object`,
+ * to get all property descriptors from it. The value is being checked against the proper `object` type.
+ * "Object that contains the properties and methods.
  * This can be an object that you created or an existing Document Object Model (DOM) object."
  * @param keys Specify which property descriptors should be returned.
  * @returns The return value is an object containing all own property descriptors of an object.
