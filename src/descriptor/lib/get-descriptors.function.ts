b16ede35974601335356c59780c191ba53d5afee
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-21 22:47:58 +0200

    docs(getDescriptors()): add default description

--- src/descriptor/lib/get-descriptors.function.ts
+++ src/descriptor/lib/get-descriptors.function.ts
@@ -2,6 +2,7 @@ import { GetDescriptors } from '../type/get-descriptors.type';
 import { ObjectPropertyDescriptors } from '../type/object-property-descriptors.type';
 /**
  * Wrapper function for the `Object` static method `getOwnPropertyDescriptors`.
+ * "Returns an object containing all own property descriptors of an object".
  * @param object "Object that contains the properties and methods.
  * This can be an object that you created or an existing Document Object Model (DOM) object."
  * @param keys Specify which property descriptors should be returned.
