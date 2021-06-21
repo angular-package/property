4c0d5d9851bd0c1ab9f5a1b484816be518f98463
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-22 00:13:31 +0200

    fix(SetSelectedDescriptor): use type from the angular-package/type

--- src/descriptor/interface/set-selected-descriptor.interface.ts
+++ src/descriptor/interface/set-selected-descriptor.interface.ts
@@ -3,7 +3,7 @@ import { Descriptor } from '../lib/descriptor.class';
 // Interface.
 import { DataDescriptor } from './data-descriptor.interface';
 // Type.
-import { ResultCallback } from '../../../../type/src/type/result-callback.type';
+import { ResultCallback } from '@angular-package/type';
 import { ThisAccessorDescriptor } from '../type/this-accessor-descriptor.type';
 /**
  * Select accessor or data descriptor instance to set.
