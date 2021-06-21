2c1035efb0e9d2b181e13a409534dfc72bbd257b
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-21 21:16:58 +0200

    refactor(getDescriptor): constrain generic `Obj` variable with the `object` type, update jsdoc

--- src/descriptor/lib/get-descriptor.function.ts
+++ src/descriptor/lib/get-descriptor.function.ts
@@ -1,11 +1,12 @@
 import { GetDescriptor } from '../type/get-descriptor.type';
 /**
  * Wrapper function for the `Object` static method `getOwnPropertyDescriptor`.
+ * Returns descriptor of the specified property from the specified object.
  * @param object "Object that contains the property."
  * @param key "Name of the property."
  * @returns The return value is a property descriptor of the specified object.
  */
-export const getDescriptor: GetDescriptor = <Obj, Key extends keyof Obj>(
+export const getDescriptor: GetDescriptor = <Obj extends object, Key extends keyof Obj>(
   object: Obj,
   key: Key
 ): PropertyDescriptor | undefined =>
