1272567e3c341e62082afe5c905bd75644784406
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-23 02:38:53 +0200

    test(Suffix): update

--- src/name/test/suffix.spec.ts
+++ src/name/test/suffix.spec.ts
@@ -1,9 +1,13 @@
-import { Suffix } from '../lib/suffix.class';
+// External import: Type.
 import { ResultCallback } from '@angular-package/type';
-
+// Import: Class.
+import { Suffix } from '../lib/suffix.class';
+/**
+ * Tests.
+ */
 describe(Suffix.name, () => {
-  let nameSuffix: Suffix;
-
+  // Constant.
+  const pattern = /[^0-9$_]/g;
   const customCallback: ResultCallback = (result: boolean, value: any): boolean => {
     if (result === true) {
       throw new Error('Result is string');
@@ -11,9 +15,17 @@ describe(Suffix.name, () => {
     return result;
   };
 
+  // Variable.
+  let nameSuffix: Suffix;
+  let suffix = 'database';
+
   beforeEach(() => nameSuffix = new Suffix());
+  beforeEach(() => suffix = 'database');
+
 
   it('is DEFINED', () => expect(nameSuffix).toBeDefined());
+
+  // Default callback.
   it('has default callback', () => {
     try {
       nameSuffix.set('$$$');
@@ -21,6 +33,8 @@ describe(Suffix.name, () => {
       expect(e.message).toEqual('Result is string');
     }
   });
+
+  // Custom callback.
   it('has custom callback', () => {
     try {
       nameSuffix.set('$$$', customCallback);
@@ -28,6 +42,21 @@ describe(Suffix.name, () => {
       expect(e.message).toEqual('Result is string');
     }
   });
-  it('initially set prefix to $$', () => expect(new Suffix('$$').get).toEqual('$$'));
-  it('set prefix to $$', () => expect(nameSuffix.set('$$').get).toEqual('$$'));
+
+  // Initial.
+  it('initially set suffix to $$', () => expect(new Suffix('$$').get).toEqual('$$'));
+
+  // Set.
+  describe(`set suffix to '${suffix}'`, () => {
+    it('default', () => expect(nameSuffix.set(suffix).get).toEqual('dat'));
+    it(`different length to ${suffix}`, () => expect(nameSuffix.length(8).set(suffix).get).toEqual(suffix));
+    it(`different pattern to ${suffix}`, () => expect(nameSuffix.pattern(pattern).set(`${suffix}27`).get).toEqual('27'));
+  });
+
+  // Define.
+  describe('define', () => {
+    it(`default to ${suffix}`, () => expect(Suffix.define(suffix)).toEqual('dat'));
+    it(`different length to ${suffix}`, () => expect(Suffix.define(suffix, undefined, 8)).toEqual('database'));
+    it(`different pattern to ${suffix}`, () => expect(Suffix.define(`${suffix}27`, pattern)).toEqual('27'));
+  });
 });
