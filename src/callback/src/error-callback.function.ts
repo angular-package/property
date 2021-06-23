544c6e639e8a24184abbf34eaa55461712a15ea4
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-23 03:25:26 +0200

    refactor(errorCallback): add `type` parameter with default value '' to choose what type of error need to be thrown

--- src/callback/src/error-callback.function.ts
+++ src/callback/src/error-callback.function.ts
@@ -2,23 +2,31 @@
 import { ResultCallback, is } from '@angular-package/type';
 // Type.
 import { ErrorCallback } from '../type/error-callback.type';
+import { ErrorType } from '../type/error-type.type';
 /**
- * Wrapper for the `ResultCallback` type function to throw an `Error` with the specified message on the specified `false` or `true` state.
- * @param message The string type value for the `Error` instance.
+ * Wrapper for the `ResultCallback` type function to throw specified type of `Error` with the specified message
+ * on the specified `false` or `true` state.
+ * @param message The string type value, as a message for the `Error` instance.
  * @param on A `boolean` state on which an `Error` should be thrown.
+ * @param type Type of error to throw - 'range, 'type', 'URI' and by default it's just an `Error`.
  * @returns The return value is a `boolean` as the result of the check.
  */
 export const errorCallback: ErrorCallback  = (
   message: string,
-  on: boolean = false
+  type: ErrorType = '',
+  on: boolean = false,
 ): ResultCallback => {
   return (result: boolean, value: any): boolean => {
+    message = `${message}, got value ${
+      is.object(value) ? JSON.stringify(value) : value
+    }`;
     if (result === on) {
-      throw new Error(
-        `${message}, got value ${
-          is.object(value) ? JSON.stringify(value) : value
-        }`
-      );
+      switch (type) {
+        case 'range': throw new RangeError(message); break;
+        case 'type': throw new TypeError(message); break;
+        case 'URI': throw new URIError(message); break;
+        default: throw new Error(message); break;
+      }
     }
     return result;
   };
