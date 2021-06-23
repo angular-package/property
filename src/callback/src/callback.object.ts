a85ca53adcfe0331b46b88d9bf8b1ebdca1d5f5b
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-23 03:26:14 +0200

    refactor(callbacks): add new callbacks `name`, `prefix`, `suffix`, `constantName`

--- src/callback/src/callback.object.ts
+++ src/callback/src/callback.object.ts
@@ -6,8 +6,14 @@ import { Callbacks } from '../interface/callbacks.interface';
  * Object with all necessary callbacks for the property package.
  */
 export const callbacks: Callbacks = {
-  accessor: errorCallback(`Accessor descriptor must be an \`ThisAccessorDescriptor<Value, Obj>\` type`),
-  data: errorCallback(`Data descriptor must be an \`DataDescriptors<Value>\` type`),
-  descriptor: errorCallback(`Any kind of descriptor was not found`),
-  getExistProperty: errorCallback(`Object with the specified key does not exist`),
+
+  accessor: errorCallback(`Accessor descriptor must be an \`ThisAccessorDescriptor<Value, Obj>\` type`, 'type'),
+  data: errorCallback(`Data descriptor must be an \`DataDescriptors<Value>\` type`, 'type'),
+  descriptor: errorCallback(`Any kind of descriptor was not found`, 'type'),
+  getExistProperty: errorCallback(`Object with the specified key does not exist`, 'type'),
+  name: errorCallback(`Name must be a \`string\` type`, 'type'),
+  prefix: errorCallback(`Prefix must be a \`string\` type`, 'type'),
+  suffix: errorCallback(`Suffix must be a \`string\` type`, 'type'),
+  constantName: errorCallback(`A \`string\` \`name\` must be initialized`, 'type')
+
 };
