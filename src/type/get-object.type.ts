7d9bbc8c8cbcee37a11c31a3e2e3102f11f60094
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-06-23 16:09:32 +0200

    style(GetObject): use `prettiezer`

--- src/type/get-object.type.ts
+++ src/type/get-object.type.ts
@@ -1,2 +1,5 @@
 import { ResultCallback } from '@angular-package/type';
-export type GetObject = <Obj extends object>(object: Obj, callback?: ResultCallback) => Obj;
+export type GetObject = <Obj extends object>(
+  object: Obj,
+  callback?: ResultCallback
+) => Obj;
