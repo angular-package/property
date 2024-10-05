85ddaaa5b2dc77c7b96e12f792ac81799550552c
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-05 18:20:16 +0000

    chore(descriptor/type): remove unnecessary.

--- src/descriptor/type/any-descriptor.type.ts
+++
@@ -1,4 +0,0 @@
-import { DataDescriptor } from '../interface/data-descriptor.interface';
-import { ThisAccessorDescriptor } from './this-accessor-descriptor.type';
-// Accessor or data descriptor.
-export type AnyDescriptor<Value, Obj = any> = ThisAccessorDescriptor<Value, Obj> | DataDescriptor<Value>;
