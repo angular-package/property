c739022c44728f41ee53e3d9f9c8506c947c2402
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-05 18:16:42 +0000

    chore(api): update to new version.

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,31 +1,22 @@
 /*
  * Public API Surface of property
  */
-// Object.
-export { get } from './lib';
-export { callbacks } from './callback';
-export { getObject } from './object';
-
-// Function.
 export {
-  getDescriptor,
-  getDescriptors,
-  getExistProperty,
-  getProperties,
-  getProperty,
-  setProperty,
+  // Class.
+  Property,
+  WrapProperty,
 } from './lib';
-export { errorCallback } from './callback';
 
-// Class.
-export { AccessorDescriptors, DataDescriptors, Descriptor } from './descriptor';
-
-// Interface.
 export {
+  // Class.
+  Descriptor,
+  Descriptors,
+  // Interface.
   AccessorDescriptor,
   CommonDescriptor,
   DataDescriptor,
-} from './descriptor/interface';
+  // Type.
+  ThisAccessorDescriptor,
+} from './descriptor';
 
-// Type.
-export { ThisAccessorDescriptor } from './descriptor/type';
+export { GetterCallback, SetterCallback } from './type';
