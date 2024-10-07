71f116fd7bf564b786bac3f9c4d05c1c09c25e94
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-07 13:04:49 +0000

    chore(descriptor/interface): export `GetSelectedDescriptor` and `SetSelectedDescriptor`.

--- src/descriptor/interface/index.ts
+++ src/descriptor/interface/index.ts
@@ -1,3 +1,5 @@
 export { AccessorDescriptor } from './accessor-descriptor.interface';
 export { CommonDescriptor } from './common-descriptor.interface';
 export { DataDescriptor } from './data-descriptor.interface';
+export { GetSelectedDescriptor } from './get-selected-descriptor.interface';
+export { SetSelectedDescriptor } from './set-selected-descriptor.interface';
