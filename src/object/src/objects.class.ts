0efebea215bb355f714fe759e5a6af9309917844
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-09 16:46:15 +0000

    refactor(Objects): add `connect()` method to add connections between objects, and `query()` to use JSON as query.

--- src/object/src/objects.class.ts
+++ src/object/src/objects.class.ts
@@ -11,42 +11,169 @@ export class Objects<
   Objs extends object,
   Names extends keyof Objs
 > {
+  static objects: any;
+
+  /**
+   * @angularpackage
+   */
+  public get connected() {
+    return this.#connected;
+  }
+
   /**
    * @angularpackage
    */
   public get get(): Objs {
-    return this.#objects;
+    return this.objects;
   }
 
   /**
-   * 
+   * @angularpackage
    */
-  readonly #objects: Objs = {} as any;
+  readonly objects: Objs = {} as any; // private 
 
+  /**
+   * @angularpackage
+   */
+  #connected: Map<Names, [PropertyKey, Names, PropertyKey]> = new Map();
 
   /**
    * Creates an instance of `Objects`.
    * @param object 
+   * @angularpackage
    */
   constructor(objects: Objs) {
-    Object.entries(objects).forEach(([key, obj]) => Object.assign(this.#objects, {[key]: new Obj(obj)}));
+    Object.entries(objects).forEach(([key, obj]) => Object.assign(this.objects, {[key]: new Obj(obj)}));
+  }
+
+  /**
+   * 
+   * @param param0 
+   * @returns 
+   * @angularpackage
+   */
+  public query<
+    Name extends Names,
+    Key extends keyof Objs[Name],
+    ToName extends Names,
+  >({}: Partial<{
+    connect: {
+      [N in Name]: {
+        [K in Key]: Partial<{ [TN in ToName]: keyof Objs[TN] | (keyof Objs[TN])[] }>
+      }
+    },
+
+    // forEach.
+    forEach: (name: Names, objects: Objs) => void,
+
+    // Get.
+    get: {
+      [N in Name]: {
+        [K in Key]: (value: Objs[N][K]) => void
+      }
+    },
+
+    // Set.
+    // set: {
+    //   [N in Name]: {
+    //     [K in Key]: Objs[N][K]
+    //   }
+    // },
+  }>) {
+    // forEach
+    if (arguments[0].forEach) {
+      (Object
+        .entries(this.objects) as [Name, Objs[Names]][])
+        .forEach(([name, obj]) => arguments[0].forEach(name, this.objects));
+    }
+
+    // Get.
+    if (arguments[0].get) {
+      (Object
+        .entries(arguments[0].get) as [Name, {
+          [K in Key]: (value: Objs[Name][K]) => void
+        }][])
+        .forEach(([name, keyCallbackFn]) => {
+          (Object
+            .entries(keyCallbackFn) as [Key, (value: Objs[Name][Key]) => void][])
+            .forEach(([key, callbackfn]) => callbackfn(Objects.objects[name].get[key]));
+        });
+    }
+
+    // Set.
+    // if (arguments[0].set) {}
+
+    // Connect.
+    if (arguments[0].connect) {
+      (Object
+        .entries(arguments[0].connect) as [Name, {
+          [K in Key]: Partial<{ [TN in ToName]: keyof Objs[TN] | (keyof Objs[TN])[] }>
+        }][])
+        .forEach(([name, keyTo]) => {
+          (Object
+            .entries(keyTo) as [Key, Partial<{ [TN in ToName]: keyof Objs[TN] | (keyof Objs[TN])[] }>][])
+            .forEach(([key, to]) => {
+              (Object
+                .entries(to) as [ToName, keyof Objs[ToName] | (keyof Objs[ToName])[]][])
+                .forEach(([toName, toKey]) => this.connect(name, key, [toName, toKey]));
+            });
+        });
+    }
+    return this;
+  }
+
+  /**
+   * 
+   * @param name 
+   * @param key 
+   * @param toName 
+   * @param toKey 
+   * @returns 
+   * @angularpackage
+   */
+  public connect<
+    Name extends Names,
+    Key extends keyof Objs[Name],
+    ToName extends Names,
+    ToKey extends keyof Objs[ToName],
+  >(
+    name: Name,
+    key: Key,
+    ...to: [toName: ToName, toKey: ToKey | ToKey[]][]
+  ): this {
+    (!Objects.objects) && (Objects.objects = this.objects);
+    to.forEach(([toName, toKey]) => {
+      (!Array.isArray(toKey)) && (toKey = [toKey]);
+      toKey.forEach(toKey => {
+        (this.objects[toName] as unknown as ObjsContainer<Objs, ToName>)!
+          .wrapProperty(
+            toKey,
+            () => Objects.objects[name].get[key],
+            value => Object.assign(Objects.objects[name].get, {[key]: value})
+          );
+        this.#addConnection(toName, toKey, name, key);
+      });
+    });
+    return this;
   }
 
   /**
    * 
    * @param name 
    * @returns 
+   * @angularpackage
    */
   public getObj<Name extends Names>(
     name: Name
   ): ObjsContainer<Objs, Name> {
-    return this.#objects[name] as unknown as ObjsContainer<Objs, Name>;
+    return this.objects[name] as unknown as ObjsContainer<Objs, Name>;
   }
 
   /**
    * Returns the value 
    * @param key 
    * @returns 
+   * @angularpackage
    */
   public getProperty<Name extends Names, Key extends keyof Objs[Name]>(
     objName: Name,
@@ -68,6 +195,30 @@ export class Objects<
     value: Objs[Name][Key]
   ): this {
     this.getObj(objName)!.setProperty(key, value);
+    return this;
+  }
+
+  /**
+   * 
+   * @param name 
+   * @param key 
+   * @param toName 
+   * @param toKey 
+   * @returns 
+   * @angularpackage
+   */
+  #addConnection<
+    Name extends Names,
+    Key extends keyof Objs[Name],
+    ToName extends Names,
+    ToKey extends keyof Objs[ToName],
+  >(
+    name: Name,
+    key: Key,
+    toName: ToName,
+    toKey: ToKey
+  ) {
+    this.#connected.set(name, [key, toName, toKey]);
     return this;
   }
 }
