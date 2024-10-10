4fd14a3da176d9704023d2b131f2b502c5ba19e8
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-10-10 10:51:41 +0000

    refactor(AnyDescriptor): & instead of |

--- src/descriptor/type/any-descriptor.type.ts
+++ src/descriptor/type/any-descriptor.type.ts
@@ -1,4 +1,4 @@
 import { DataDescriptor } from '../interface/data-descriptor.interface';
 import { ThisAccessorDescriptor } from './this-accessor-descriptor.type';
 // Accessor or data descriptor.
-export type AnyDescriptor<Value, Obj extends object = object> = ThisAccessorDescriptor<Value, Obj> | DataDescriptor<Value>;
+export type AnyDescriptor<Value, Obj extends object = object> = ThisAccessorDescriptor<Value, Obj> & DataDescriptor<Value>;
