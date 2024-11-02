ad46aea19c53922c1b6997163c64b2241e180c5a
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-11-02 22:03:36 +0000

    refactor(descriptor/interface/index): add type indicator.

--- src/descriptor/interface/index.ts
+++ src/descriptor/interface/index.ts
@@ -1,5 +1,5 @@
-export { AccessorDescriptor } from './accessor-descriptor.interface';
-export { CommonDescriptor } from './common-descriptor.interface';
-export { DataDescriptor } from './data-descriptor.interface';
-export { GetSelectedDescriptor } from './get-selected-descriptor.interface';
-export { SetSelectedDescriptor } from './set-selected-descriptor.interface';
+export type { AccessorDescriptor } from './accessor-descriptor.interface';
+export type { CommonDescriptor } from './common-descriptor.interface';
+export type { DataDescriptor } from './data-descriptor.interface';
+export type { GetSelectedDescriptor } from './get-selected-descriptor.interface';
+export type { SetSelectedDescriptor } from './set-selected-descriptor.interface';
