a2676c8032844ffe9e39f7a952167399913e3ff5
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-11-04 06:03:07 +0000

    refactor(type): add `previousGetValue` and value in `GetterCallback`, and `previousValue` in `SetterCallback`.

--- src/type/getter-callback.type.ts
+++ src/type/getter-callback.type.ts
@@ -1,5 +1,7 @@
 export type GetterCallback<Obj, Key extends keyof Obj> = (
   this: Obj,
   key: Key,
+  previousGetValue: Obj[Key],
+  value: Obj[Key],
   instance: Obj
 ) => Obj[Key] | void;
--- src/type/setter-callback.type.ts
+++ src/type/setter-callback.type.ts
@@ -1,7 +1,7 @@
 export type SetterCallback<Obj, Key extends keyof Obj> = (
   this: Obj,
   value: Obj[Key],
-  oldValue: Obj[Key],
+  previousValue: Obj[Key],
   key: Key,
   instance: Obj
 ) => void;
